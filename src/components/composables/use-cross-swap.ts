5a7f403173e1142236872d9428a78141309d921d
Bilal <bilal.mte@gmail.com>
2021-08-21 20:44:59 +0700

    fix supported assets in swap

--- src/components/composables/use-cross-swap.ts
+++ src/components/composables/use-cross-swap.ts
@@ -3,13 +3,20 @@ import * as constants from '@/constants'
 import useEcocWallet from './use-ecoc-wallet'
 import useEthWallet from './use-eth-wallet'
 
+const ecocSwapSupported = ['ECOC', 'EFG']
+const ethSwapSupported = ['ECOC', 'EFG']
+
 export default function useCrossSwap() {
   const { assets: ecocAssets } = useEcocWallet()
   const { assets: ethAssets } = useEthWallet()
 
   const supportedAssets = reactive({
-    [constants.TYPE_ECOC]: ecocAssets,
-    [constants.TYPE_ETH]: ethAssets.value.filter((asset) => !(asset.symbol === constants.ETH)),
+    [constants.TYPE_ECOC]: ecocAssets.value.filter((asset) =>
+      ecocSwapSupported.includes(asset.symbol)
+    ),
+    [constants.TYPE_ETH]: ethAssets.value.filter((asset) =>
+      ethSwapSupported.includes(asset.symbol)
+    ),
   })
 
   return {
